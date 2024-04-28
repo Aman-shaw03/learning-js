2ca014ffcaac20b24715eace1d69ca9321f227ee
Aman shaw <142434955+Aman-shaw03@users.noreply.github.com>
2024-04-28 17:03:12 +0000

    datatype conversions

---
+++ 01_basics/03_dataconversion.js
@@ -0,0 +1,37 @@
+let score = 33
+
+//console.log(typeof score);
+let age = "25"
+//console.log(typeof age);
+let valueInnumber = Number(age)
+//console.log(typeof valueInnumber);
+
+let number = "33abhad"
+//console.log(typeof number);
+let numbervalue = Number(number)
+//console.log(numbervalue);
+
+// true => 1; false => 0
+// "abc" => NaN
+// "123" => 123
+// Null => object 
+// undefined => undefined
+
+let isLoggedIn = undefined
+
+let boolloggedIn = Boolean(isLoggedIn)
+
+//console.log(boolloggedIn);
+
+// 1=> true; 0=> false
+// "" => false
+// "Aman" => true
+// 123 => true
+// null , undefined => false
+
+let val = true
+
+let stringval = String(val)
+
+console.log(stringval);
+console.log(typeof stringval);
