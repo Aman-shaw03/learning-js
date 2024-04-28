4a420d27f98a17f879d0d77fdea98eacc015af99
Aman shaw <142434955+Aman-shaw03@users.noreply.github.com>
2024-04-28 09:36:30 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all js code as newer versions
+    // alert(3+3)
+// we are using nodejs not browser
+
+console.log(3+3)
+
+console.log("Hello Aman");
+
+// Data types;
+
+// string => "Aman" , "shaw"
+// number => 2 to the power of 53
+// bigint
+// boolean => true or false
+// undefined => undefined
+// null is a data type also a value 
+console.log(typeof null);
+// this will show type as object *imp for knowledge
+//object
