c7000c6598c73bcc297fde4cce8c35e2e60a92c1
Aman shaw <142434955+Aman-shaw03@users.noreply.github.com>
2024-05-02 15:18:48 +0000

    string

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,20 @@
+let name = "Aman"
+let repoCount = 6
+// console.log(name + " has " + repoCount + ' repository' ); outdated
+
+// console.log(`Hello my name is ${name} and i have ${repoCount} Repositories in Github.`);
+
+const gameName = new String('Detroit')
+// by declaring a string with new String('...') it provides us with various function and methods with the  string..check it on Console
+// console.log(gameName[0]);
+// console.log(gameName[1]);
+// console.log(gameName.slice(0,4));
+// console.log(gameName.toLowerCase());
+// console.log(gameName.toUpperCase());
+// console.log(gameName.trimEnd());
+// console.log(gameName.charAt(5));
+// console.log(gameName.length);
+// console.log(gameName.substring(2,6));
+
+const stringOne = new String('MAx-The-underworld-king')
+console.log(stringOne.split('-').indexOf('king'));
