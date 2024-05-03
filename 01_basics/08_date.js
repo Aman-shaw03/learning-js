88f50e8427406ca51dc33d29e9547a6ca0d621de
Aman shaw <142434955+Aman-shaw03@users.noreply.github.com>
2024-05-03 10:00:13 +0000

    more dates practise

--- 01_basics/08_date.js
+++ 01_basics/08_date.js
@@ -30,12 +30,38 @@ let currentDate = Date.now()
 // console.log(currentDate);
 
 let newDate = new Date()
-console.log(newDate.getMonth());
+// console.log(newDate.getMonth());
 
-console.log(newDate.toLocaleString('Default',{
-    weekday: 'long',
-    minute: 'numeric',
-    timeZone: 'GMT',
-    timeZoneName: 'long'
-}));
+// console.log(newDate.toLocaleString('Default',{
+//     weekday: 'long',
+//     minute: 'numeric',
+//     timeZone: 'GMT',
+//     timeZoneName: 'long'
+// }));
+
+// let now = new Date();
+// let hours = now.getHours();
+// let minutes = now.getMinutes();
+// let seconds = now.getSeconds();
+// console.log(`Current Time: ${hours}:${minutes}:${seconds}`);
+
+let today = new Date();
+let year = today.getFullYear();
+let month = String(today.getMonth() + 1).padStart(2, '0');
+let day = String(today.getDate()).padStart(2, '0');
+let hours = String(today.getHours()).padStart(2, '0');
+let minutes = String(today.getMinutes()).padStart(2, '0');
+let seconds = String(today.getSeconds()).padStart(2, '0');
+
+// console.log(`Current Date and Time: ${year}-${month}-${day} ${hours}:${minutes}:${seconds}`);
+
+
+let abhiDate = new Date()
+// console.log(abhiDate.toLocaleString());
+
+let birthyear = new Date("03-11-1999")
+let todayyear = new Date()
+let todayyear0 = todayyear.getFullYear()
+
+console.log(`My current age is ${(todayyear.getFullYear())-(birthyear.getFullYear())}`);
 
