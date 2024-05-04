6a463a6c54f5c9d3cd95c8270e425251cb5db536
Aman shaw <142434955+Aman-shaw03@users.noreply.github.com>
2024-05-04 17:43:54 +0000

    object part 2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,44 @@
+// const tinderuser = new Object()
+// singleton object
+const tinderuser = {}
+
+tinderuser.id ="123abc"
+tinderuser.name = "Aman"
+tinderuser.isLoggedIn = false
+tinderuser.age = 24
+// console.log(tinderuser);
+
+const regularUser = {
+    email: "aman@shaw.com",
+    fullname: {
+        username: {
+            firstName : "AMan",
+            lastName : "Shaw"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.username);
+
+const obj1 ={1:"a",2:"b"}
+const obj2 ={3:"a",4:"b"}
+const obj3 ={5:"a",6:"b"}
+
+// how to assign all 3 Object in 1 object.
+
+const obj4 = Object.assign({},obj1,obj2,obj3)
+// Object.assign takes 2 values target and sources , its good practise to give {} as source --so it will assign all obj to that empty {} and then assign it to obj4
+
+// best way to assign is to use SPREAD
+
+const obj5 = {...obj1,...obj2,...obj3}
+console.log(obj4);
+console.log(obj5);
+
+console.log(Object.keys(tinderuser));
+console.log(Object.values(tinderuser));
+console.log(Object.entries(tinderuser));
+// all the results are in Array
+
+//check whether a object has a specific Key
+console.log(tinderuser.hasOwnProperty("age"));
