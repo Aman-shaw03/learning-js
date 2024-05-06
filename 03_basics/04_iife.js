b223aa06c7fa783cb6f35b36583e6361b9940d57
Aman shaw <142434955+Aman-shaw03@users.noreply.github.com>
2024-05-06 17:44:38 +0000

    .

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -3,7 +3,13 @@
 /* Interview Q. why IIFE? */
 /* Global scope ke pollution se problem hoti hai kai baar, ussi pollution(variables) ko hatane ke lia IIFE use krte hai */
 
-// (function chai(){
-//     console.log(`DB Connected`);
-// })()
+(function chai(){
+    //Named IIFE , since we give it a name
+    console.log(`DB Connected`);
+})(); // always remember to explicitly use ";"
+
+((name) => {
+    // normal IIFE
+    console.log(`DB Connected TWO ${ name}`);
+})("Aman");
 
