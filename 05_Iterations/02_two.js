67037e96b66c590aceb857dac7af29320ea9f0a5
Aman shaw <142434955+Aman-shaw03@users.noreply.github.com>
2024-05-12 07:33:47 +0000

    while and DO while Loops

---
+++ 05_Iterations/02_two.js
@@ -0,0 +1,26 @@
+// while loops
+
+let index = 0
+
+while (index < 10) {
+    // console.log(`value of while loop is ${index} `);
+    index = index + 1
+}
+
+let myArr = ["Dhoni","Rohit","Kohli"]
+let arr = 0
+
+while (arr < myArr.length) {
+    // console.log(myArr[arr]);
+    arr = arr +1
+    
+}
+
+// do while loop
+
+let score = 12
+
+do {
+    console.log(`Value of score is ${score}`);
+    score++
+} while (score < 11);
